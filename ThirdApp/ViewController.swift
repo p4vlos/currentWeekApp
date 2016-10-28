ae411458842cbc3b9e97580ba9814660b0d45b3e
Pavlos Nicolaou <nicolaou_paul@hotmail.com>
2016-10-28 14:50:41 +0100

    finished app and shows the current week

--- ThirdApp/ViewController.swift
+++ ThirdApp/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,20 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func weekOfYear(_ sender: Any) {
+        // Log the week value number
+        let date = datePicker.date
+        let week = datePicker.calendar.component(.weekOfYear, from: date)
+        print("week \(week)")
+        
+        // Pop-up a UIAlertController
+        let alert = UIAlertController(title: "Week of Year", message: "week \(week)", preferredStyle: .alert)
+        let close = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alert.addAction(close)
+        self.present(alert, animated: true, completion: nil)
+        
+    }
+    
 
 }
 
